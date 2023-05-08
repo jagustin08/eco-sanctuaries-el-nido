b5a14f1045e027858ed1e3c36b42edd98ff5173d
Jayson Agustin <dev.jagustin@gmail.com>
2023-05-08 16:18:00 +0800

    remove files

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Eco Sanctuaries El Nido</title>
+  <link rel="stylesheet" href="src/css/main.css" />
+</head>
+
+<body>
+  <?php @include("src/includes/header.php"); ?>
+
+  <main>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque velit culpa vero quo maiores quaerat dolores facilis earum, reiciendis unde iste veniam dolorum cupiditate numquam assumenda inventore excepturi quae minima?</p>
+  </main>
+
+  <?php @include("src/includes/footer.php"); ?>
+  <script src="src/js/main.js"></script>
+</body>
+
+</html>
