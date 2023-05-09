23f20e727b58477ece1b7d0b985bfd28997e7d20
Jayson Agustin <dev.jagustin@gmail.com>
2023-05-09 13:36:13 +0800

    add images

--- index.php
+++ index.php
@@ -5,55 +5,34 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta property="og:title" content="Eco Sanctuaries El Nido" />
+  <meta property="og:description" content="Eco Sanctuaries is a nature lodge and retreat sanctuary in Nacpan Beach, El Nido Palawan offering accommodations, culinary offerings, nature-based and wellness experiences." />
+  <meta property="og:url" content="https://www.ecosanctuarieselnido.com/" />
+  <meta property="og:image" content="https://www.ecosanctuarieselnido.com/src/images/bg_home.jpg" />
+
   <title>Eco Sanctuaries El Nido</title>
+
+  <link rel="apple-touch-icon" sizes="180x180" href="src/images/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="src/images/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="src/images/favicon/favicon-16x16.png">
+  <link rel="manifest" href="src/images/favicon/site.webmanifest">
+
+  <link rel="stylesheet" href="node_modules/swiper/swiper-bundle.min.css">
   <link rel="stylesheet" href="src/css/main.css">
 </head>
 
 <body>
-  <h1>Hello World</h1>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda explicabo excepturi ex illum aut ad quasi maxime nostrum magni ea? Necessitatibus porro corrupti laboriosam hic dolorum, sequi rerum quae blanditiis!</p>
+  <?php include "includes/header.php"; ?>
 
-  <div class="accordion" id="accordionExample">
-    <div class="accordion-item">
-      <h2 class="accordion-header">
-        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          Accordion Item #1
-        </button>
-      </h2>
-      <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-        <div class="accordion-body">
-          <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-        </div>
-      </div>
-    </div>
-    <div class="accordion-item">
-      <h2 class="accordion-header">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-          Accordion Item #2
-        </button>
-      </h2>
-      <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-        <div class="accordion-body">
-          <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-        </div>
-      </div>
-    </div>
-    <div class="accordion-item">
-      <h2 class="accordion-header">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          Accordion Item #3
-        </button>
-      </h2>
-      <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-        <div class="accordion-body">
-          <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-        </div>
-      </div>
-    </div>
-  </div>
+  <main>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae nam maxime animi aperiam impedit quas veniam ipsum dolore? Nam, ex. Earum accusantium neque enim saepe quisquam tempora quidem a eveniet?</p>
+  </main>
 
-  <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js" defer></script>
-  <script src="src/js/main.js" defer></script>
+  <?php include "includes/footer.php"; ?>
+  <script src="node_modules/swiper/swiper-bundle.min.js"></script>
+  <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="src/js/main.js"></script>
 </body>
 
 </html>
