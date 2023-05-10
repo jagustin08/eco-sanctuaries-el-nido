9f0a4910ae7182365be628cc431572847a8a638f
Jayson Agustin <dev.jagustin@gmail.com>
2023-05-11 01:56:48 +0800

    add header and footer links

--- includes/footer.php
+++ includes/footer.php
@@ -1,25 +1,25 @@
-<footer class="bg-dark text-light text-center text-lg-unset fs-7">
+<footer id="contact-us" class="bg-dark text-light text-center text-lg-unset fs-7">
     <div class="container py-5">
         <div class="row">
             <div class="col-lg-6 mb-4 mb-lg-0">
                 <h5 class="mb-3">Get In Touch</h5>
                 <p class="mb-1">Eco Sanctuaries, Nacpan Beach, El Nido, Palawan, Philippines</p>
-                <a href="#" class="d-block mb-1 text-beige">ecosanctuarieselnido@gmail.com</a>
+                <a href="mailto: ecosanctuarieselnido@gmail.com" class="d-block mb-1 text-beige">ecosanctuarieselnido@gmail.com</a>
                 <p class="mb-0">+63993 915 8457</p>
             </div>
 
             <div class="col-lg-6 mb-4 mb-lg-0">
                 <ul class="nav flex-column flex-lg-row justify-content-lg-end gap-2 gap-lg-3 mb-4 mb-lg-2">
-                    <li class="nav-item"> <a href="#" class="nav-link text-light text-beige-hover p-0">Home</a> </li>
-                    <li class="nav-item"> <a href="#" class="nav-link text-light text-beige-hover p-0">About</a> </li>
-                    <li class="nav-item"> <a href="#" class="nav-link text-light text-beige-hover p-0">Nature Lodges</a> </li>
-                    <li class="nav-item"> <a href="#" class="nav-link text-light text-beige-hover p-0">Experiences</a> </li>
-                    <li class="nav-item"> <a href="#" class="nav-link text-light text-beige-hover p-0">Book now</a> </li>
+                    <li class="nav-item"> <a href="https://www.ecosanctuarieselnido.com/" class="nav-link text-light text-beige-hover p-0">Home</a> </li>
+                    <li class="nav-item"> <a href="about.php" class="nav-link text-light text-beige-hover p-0">About</a> </li>
+                    <li class="nav-item"> <a href="nature_lodges.php" class="nav-link text-light text-beige-hover p-0">Nature Lodges</a> </li>
+                    <li class="nav-item"> <a href="experiences.php" class="nav-link text-light text-beige-hover p-0">Experiences</a> </li>
+                    <li class="nav-item"> <a href="https://staahmax.staah.net/be/index_be?propertyId=NTY5OQ==&individual=true" class="nav-link text-light text-beige-hover p-0" target="_blank">Book now</a> </li>
                 </ul>
 
                 <ul class="nav justify-content-center justify-content-lg-end gap-3 gap-lg-4 align-items-center">
-                    <li class="nav-item"> <a href="#" class="nav-link text-light text-beige-hover p-0 fs-4"> <i class="fa-brands fa-facebook"></i></a> </li>
-                    <li class="nav-item"> <a href="#" class="nav-link text-light text-beige-hover p-0 fs-2"> <i class="fa-brands fa-instagram"></i> </a> </li>
+                    <li class="nav-item"> <a href="https://www.facebook.com/ecosanctuarieselnido" target="_blank" class="nav-link text-light text-beige-hover p-0 fs-4"> <i class="fa-brands fa-facebook"></i></a> </li>
+                    <li class="nav-item"> <a href="https://www.instagram.com/ecosanctuaries.elnido/" target="_blank" class="nav-link text-light text-beige-hover p-0 fs-2"> <i class="fa-brands fa-instagram"></i> </a> </li>
                 </ul>
             </div>
         </div>
--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,8 @@
 <header>
     <nav class="navbar navbar-expand-lg fixed-top bg-dark" aria-label="Offcanvas navbar large" data-bs-theme="dark">
         <div class="container">
-            <a class="navbar-brand mx-auto me-lg-auto" href="#" style="width: 120px;">
-                <img src=" src/images/logo.png" alt="Eco Sanctuaries logo" class="img-fluid">
+            <a href="https://www.ecosanctuarieselnido.com/" class="navbar-brand mx-auto me-lg-auto" style="width: 120px;">
+                <img src="src/images/logo.png" alt="Eco Sanctuaries logo" class="img-fluid">
             </a>
             <button class="navbar-toggler shadow-none border-0 position-absolute" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar2" aria-controls="offcanvasNavbar2" aria-label="Toggle navigation" style="right: 12px">
                 <span class="navbar-icon d-block bg-white opacity-50" style="height: 2px; width: 25px"></span>
@@ -11,28 +11,28 @@
             </button>
             <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar2" aria-labelledby="offcanvasNavbar2Label">
                 <div class="offcanvas-header">
-                    <img src=" src/images/logo.png" alt="Eco Sanctuaries logo" class="img-fluid mx-auto" style="width: 120px;">
+                    <a href="https://www.ecosanctuarieselnido.com/" class="mx-auto" style="width: 120px;"><img src="src/images/logo.png" alt="Eco Sanctuaries logo" class="img-fluid"></a>
                     <button type="button" class="btn-close btn-close-white m-0 shadow-none" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
                 <div class="offcanvas-body">
                     <ul class="navbar-nav justify-content-end flex-grow-1 gap-lg-3">
                         <li class="nav-item">
-                            <a class="nav-link text-beige-hover" href="#">Home</a>
+                            <a class="nav-link text-beige-hover" href="https://www.ecosanctuarieselnido.com/">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-beige-hover" href="#">About</a>
+                            <a class="nav-link text-beige-hover" href="about.php">About</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-beige-hover" href="#">Nature Lodges</a>
+                            <a class="nav-link text-beige-hover" href="nature_lodges.php">Nature Lodges</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-beige-hover" href="#">Experiences</a>
+                            <a class="nav-link text-beige-hover" href="experiences.php">Experiences</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-beige-hover" href="#">Contact us</a>
+                            <a class="nav-link text-beige-hover" href="#contact-us">Contact us</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-beige-hover" href="#">Book now</a>
+                            <a class="nav-link text-beige-hover" href="https://staahmax.staah.net/be/index_be?propertyId=NTY5OQ==&individual=true" target="_blank">Book now</a>
                         </li>
                     </ul>
                 </div>
