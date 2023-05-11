f8220bbc1fd822ad247fd67874c848172d136773
Jayson Agustin <dev.jagustin@gmail.com>
2023-05-12 03:06:25 +0800

    add faqs

--- nature_lodges.php
+++ nature_lodges.php
@@ -186,6 +186,149 @@
           </div>
         </div>
     </section>
+
+    <section>
+      <div class="container pt-7">
+        <h2 class="display-6 font-cormorant fw-bold mb-7 text-center">General Notes and Reminders about <br>Accommodations</h2>
+        <div class="row">
+          <div class="col-lg-6 mb-3 mb-lg-0">
+            <ul class="vstack gap-3 mb-0" style="padding-left: 2rem">
+              <li>Regular published rate is for 2 people. The lodge with sea view balcony can host up to 4 people at an additional charge of Php1500/person, with 2 queen beds. The cabin is best for 2 people with 1 queen bed.</li>
+              <li>Our lodges are all screened and protected from mosquitos, and we also have additional mosquito net draped around the bed when needed. However, bear in mind that we have a mountain behind us and are subject to nature&apos;s elements. You may not see them as they avoid humans, but there may be a harmless endemic gecko which you can hear sometimes at night, or sandflies by the beach depending on the season which is common in some of El Nido&apos;s beaches such as Nacpan. They are harmless. If you are sensitive to them, you can bring insect repellant for protection and we also sell repellants, and our accommodations are screened and protected with mosquito net drapings in the rooms as well, but if you are highly sensitive this may not be the best place for you. We have had a lot of children stay, and have been unharmed by them, but do remember we are in a natural setting.</li>
+              <li>We are not a luxury resort. We do not have televisions, refrigerators in the rooms, heater for showers, and airconditioning. Our accommodations are designed to be breezy and maximize air flow, fresh air, and the ocean breeze, and are equipped with stand fans.</li>
+              <li>We have free purified water for all guests and in room coffee and tea accompanied by a thermos for hot water which is refilled daily, but not an electric devise.</li>
+            </ul>
+          </div>
+          <div class="col-lg-6">
+            <ul class="vstack gap-3 mb-0" style="padding-left: 2rem">
+              <li>We have satellite wifi which are free to use for all guests but do not have signal from your cell provider given that we have a mountain behind us blocking the signal. The wifi is best for communications, emailing, social media.</li>
+              <li>We do not currently allow newborns or those less than 2 years old, given that we understand that there may be more sensitivities to natural elements or allergies. We also do not have amenities such as cribs for babies. Thus, we do not accept. We can only accept shall a waiver be signed by the parents.</li>
+              <li>Please add after the 5th bullet point: Given that our solar systems have maximum wattage capacity to ensure electricity 24/7 for the accommodations and no power disruptions, we hope you understand that appliances such as your own blowdryers, hair straighteners, electric kettles, and other heating devices with high wattage that can cause our system to overload may not be used.</li>
+              <li>We are located in the more remote side of Nacpan beach and there is no road access to us. We arrange transfers from airport or town direct to our place through a 4x4 pickup for additional fee. For those going on your own through scooter or dropoff in the Nacpan public beach parking/entrance through your own van, please note that there is walking by the beach for about 10 minutes to get to us. Our staff can help with luggage and meet you if needed. We can help with all arrangements.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="container py-7">
+        <h2 class="display-6 font-cormorant fw-bold mb-7 text-center">FAQ's</h2>
+        <div class="accordion" id="accordionExample">
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="headingOne">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                Food and drink
+              </button>
+            </h2>
+            <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+              <div class="accordion-body">
+                <ul>
+                  <li>What time is breakfast served?</li>
+                  <p>A la carte breakfast is served daily from 8:00-10:00AM. If leaving early for a tour the next day, we can do 7:30AM and leave your advanced orders with the waiter the previous day to inform the kitchen.</p>
+                  <li>Can you cater to special dietary needs?</li>
+                  <p>Our restaurant offers a variety of dishes suitable for veg/non vegetarian preferences, and also kid-friendly meals. Our kitchen can customize meals however it is preferred that if you have strict special dietary needs you inform us ahead. Do note that we are in a remote location and we would need advanced notice if there are specialized equests.</p>
+                  <li>Can we bring food and cook/have your chef cook?</li>
+                  <p>We do not have cooking services or a kitchen that guests can use as indicated in our amenities. Only food ordered from the restaurant is allowed.</p>
+                  <li>Are we allowed to bring our own alcoholic beverages?</li>
+                  <p>In house guests may bring your own bottles at no corkage. However, please understand that we strive for a peaceful stay for our guests and excessive drinking and noise is not allowed.
+                  </p>
+                  <li>Do you serve sodas?</li>
+                  <p>Given that we encourage healthier options in our restaurant, we do not serve sodas but freshly prepared juices and shakes as non-alcoholic beverages.</p>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="headingTwo">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Getting around
+              </button>
+            </h2>
+            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+              <div class="accordion-body">
+                <ul>
+                  <li>Where can we rent a scooter/hire a tricycle?</li>
+                  <p>You can rent a scooter from the public beach entrance past the I Love Nacpan sign, about a 15 minute walk from us by the beach. Just ask where scooters can be rented/where you can book tricycles. Scooter is Php600/day.</p>
+                  <li>What are the tours/experiences we can offer?</li>
+                  <p>Please check our Experiences tab.</p>
+                  <li>What are the places we can visit nearby with a scooter?</li>
+                  <ul>
+                    <li>We recommend for you to go to Bulalacao Falls, which is just about 30 minutes from here. It is an easy walk crossing rivers in a forested area. The total walk is 45 minutes one way.</li>
+                    <li>f you like hiking, you can hike Mt. Mansilawit which is around 30 minutes from here. It is a 2.5-3 hour hike roundtrip. </li>
+                    <li>Further north, you can visit Ille Cave and the Dewil Mangrove River.</li>
+                    <li>We can arrange private transport as well to take you to these places.</li>
+                  </ul>
+                  <li>What are the options for transportation?</li>
+                  <ul>
+                    <li>Private 4x4 pickup transfer direct to our place which drives through the beach php2300/one way </li>
+                    <li>Public shuttle van that drops you off in Sunmai parking lot, from there you walk 15 minutes by the beach to our place. Php500/person. They have regular departures from town but not hourly.</li>
+                    <li>Private van Php1500/way, drops you off in our private parking and you walk to us &lt; 10 mins by the beach.</li>
+                    <li>Bring your own scooter/rent from town or hire a tricycle</li>
+                  </ul>
+                  <li>How do we go to Puerto Princesa?</li>
+                  <ul>
+                    <li>There are 2 ways: Private van which costs Php8000, or join the public shuttle vans that depart hourly from El Nido town van terminal the cost is around Php700-Php800/person. You will just need to go to the terminal and book your spot there, we cannot arrange the public van departures. </li>
+                    <li>Puerto Princesa is about a 5.5 hour land travel from Nacpan.</li>
+                  </ul>
+                  <li>How do we go to Coron?</li>
+                  <p>You can book a ticket online through Montenegro Shipping lines and look at their schedules of departures. You will go to El Nido port in town and from there take the ferry about 5 hours to Coron.</p>
+                  <li>Can you book our flights?</li>
+                  <p>As of now we do not arrange flights. You can book online direct flight from El Nido to Manila through Airswift Airlines.</p>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="headingThree">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                Planning your stay
+              </button>
+            </h2>
+            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+              <div class="accordion-body">
+                <ul>
+                  <li>Are there ATMs nearby?</li>
+                  <p>There are no ATMs nearby and best to bring cash already prior to your arrival. Credit card payments are accepted through PayPal but with a 5% charge for PayPal and interbank fees.</p>
+                  <li>Can we do long term stays?</li>
+                  <p>Long term stays are allowed. Email <a href="mailto:ecosanctuarieselnido@gmail.com" class="text-beige">ecosanctuarieselnido@gmail.com</a> for discounted rates and to coordinate.</p>
+                  <li>What are your refund and cancellation policies?</li>
+                  <p>The booking is non-refundable if cancelled within 30 days prior to arrival, and may only be rescheduled or refunded due to force majeure and natural calamities.</p>
+                  <li>Are we allowed to bring pets? </li>
+                  <p>Pets are allowed, however not on any of the beds and must be leashed on all times within the vicinity. There is a pet charge of Php1,000/night. An additional P2,000 cash security deposit will be required upon check-in for damages or stains incurred by pets, if any and will be returned if not used. Any damages to furniture assessed by team from bites of pets or scratches will have to be charged the full amount for brand new replacement.</p>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="headingFour">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                During your stay
+              </button>
+            </h2>
+            <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+              <div class="accordion-body">
+                <ul>
+                  <li>Is smoking allowed?</li>
+                  <p>Smoking is prohibited within the property and only allowed outside of the property, but cigarette butts must be disposed of properly. Only vape is allowed within the restaurant/property but must not be blown directly to guests.</p>
+                  <li>Can we do bonfire?</li>
+                  <p>Bonfires are not allowed as most of our infrastructure makes use of native materials and we are near a forest.</p>
+                  <li>Do you do laundry service?</li>
+                  <p>We can do your laundry for Php100/kilo. We will wash and dry them here. </p>
+                  <li>Until what time is housekeeping?</li>
+                  <p>Housekeeping is available until 6:00PM.</p>
+                  <li>Do you have yoga sessions?</li>
+                  <p>As of now the yoga is by request for private session and we will check with teacher on availability. It is Php3500 for up to 5 people. But we are working to have daily yoga here in the sanctuary soon.</p>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 
   <?php include "includes/footer.php"; ?>
