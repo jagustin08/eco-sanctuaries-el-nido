c6d2664cd0387c9d4c80f217b958f9d0784e7b3d
Jayson Agustin <dev.jagustin@gmail.com>
2023-05-12 12:25:52 +0800

    add experience

--- experiences.php
+++ experiences.php
@@ -26,7 +26,50 @@
   include "includes/header.php"; ?>
 
   <main>
-    <section style="background: url('src/images/bg_home.jpg') no-repeat center top;" class="min-vh-100">
+    <section class="min-vh-100 pt-10 pb-3 text-center text-white d-grid align-items-center" style="background: url('src/images/bg_experiences.jpg') no-repeat center center; background-size: cover">
+      <div class="container">
+        <h1 class="display-5 font-cormorant fw-bold mb-4">Eco Sanctuaries Experiences</h1>
+        <div class="col-lg-5 mx-auto">
+          <p class="mb-10">Discover paradise through a variety of experiences in and out of the sanctuary.</p>
+          <a href="#" class="btn btn-beige text-uppercase fs-7 fw-bold rounded-0 font-cormorant" style="width: 200px; --bs-btn-padding-y: 13px;">Book now</a>
+        </div>
+      </div>
+    </section>
+
+    <section class="text-center d-grid align-items-center py-7">
+      <div class="container">
+        <div class="col-lg-10 mx-auto pb-7">
+          <h2 class="display-6 font-cormorant fw-bold mb-4">Experiences within the Sanctuary</h2>
+          <p class="mb-3">We connect guests to healing experiences that allow them to go within through activities such as yoga, sound, healing, and breathwork. We offer community-based and nature-based experiences that allow them to harness the healing powers of nature, while experiencing our biodiverse island and supporting local livelihood.</p>
+          <p class="mb-0">We have an in house Kawan Restaurant and Bar (Kawan meaning flock of birds in native language) where the menu was crafted intentionally for guests to taste local and international flavors while using local, fresh, garden-grown produce. Through our restaurant, we are able to offer authentic, personal culinary experiences for our guests and visitors of Nacpan Beach. Private dining experiences and intimate banquets may be catered to. </p>
+        </div>
+
+        <div class="row g-3">
+          <div class="col-md-4">
+            <img src="src/images/experience/experience_1.jpg" alt="" class="img-fluid">
+          </div>
+          <div class="col-md-4">
+            <img src="src/images/experience/experience_2.jpg" alt="" class="img-fluid">
+          </div>
+          <div class="col-md-4">
+            <img src="src/images/experience/experience_3.jpg" alt="" class="img-fluid">
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="row g-0">
+        <div class="col-lg-6">
+          <img src="src/images/experience/yoga_1.jpg" alt="" class="img-fluid h-100 object-fit-cover">
+        </div>
+        <div class="col-lg-6 bg-beige p-3 p-lg-7 d-grid align-content-center justifcy-content-center">
+          <h3 class="text-uppercase fw-medium fs-7 mb-2" style="letter-spacing: 6px">Wellness</h3>
+          <h4 class="fs-2 fw-bold font-cormorant mb-4">Yoga and mindfulness retreats</h4>
+          <p class="mb-3">Our space was designed as a venue for people to come together and come in contact with nature and themselves through yoga and mindfulness retreats.</p>
+          <p class="mb-0">We customize programs with our partner SoulSpeak Mindfulness and Yoga Retreats, and also welcome other groups who wish to hold space in our sanctuary. For pre-scheduled multi-day retreats, you may view the schedules posted on our pages. Contact us if you want to collaborate.</p>
+        </div>
+      </div>
     </section>
   </main>
 
